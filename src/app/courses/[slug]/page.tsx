6d24c6bd53c26612430c0df3587a10621a508342
ilhamhafidz404 <xxspanzie@gmail.com>
2024-12-04 22:30:26 +0700

    ICF-30: Course Detail Page (on progress)

---
+++ src/app/courses/[slug]/page.tsx
@@ -0,0 +1,219 @@
+import { IconCalendar, IconCalendarFill, IconPeopleFill } from "justd-icons";
+import React from "react";
+
+export default function DetailCoursePage() {
+  return (
+    <div className="xl:px-20 md:px-10 px-5">
+      <section className="bg-indigo-500 h-[300px] w-full rounded-md mt-10 mx-auto flex items-end p-5 text-white">
+        <div>
+          <h1 className="text-3xl font-semibold uppercase mb-3">
+            MATKUL INDONESIA Kelas XII RPL
+          </h1>
+          <p className="text-xl text-gray-100">Ilham Hafidz</p>
+        </div>
+      </section>
+      <section className="grid xl:grid-cols-4 mt-10 gap-5">
+        <div className="relative">
+          <div className="border p-4 rounded bg-white sticky top-5 w-full">
+            <div className="flex gap-2 items-center mb-5">
+              <span className="w-[55px] h-[55px] rounded-full bg-indigo-500"></span>
+              <div>
+                <p className="text-xl font-medium">Nama Guru</p>
+                <p className="text-sm text-gray-800 italic">08758956544354</p>
+              </div>
+            </div>
+            <div className="flex xl:flex-col md:flex-row flex-col gap-5 justify-between xl:items-start items-center">
+              <div className="w-full">
+                <p className="text-sm italic text-gray-800 mb-5">
+                  <span className="flex gap-1 items-center">
+                    <IconCalendarFill className="size-5" />
+                    Dibuat Pada :
+                  </span>
+                  <span className="text-base text-gray-900 not-italic">
+                    12 November 2023
+                  </span>
+                </p>
+                <p className="text-sm italic text-gray-800">
+                  <span className="flex gap-1 items-center">
+                    <IconPeopleFill className="size-5" />
+                    Diikuti Oleh :
+                  </span>
+                  <span className="text-base text-gray-900 not-italic">
+                    200+ Siswa
+                  </span>
+                </p>
+              </div>
+              <div className="w-full">
+                <button className="bg-indigo-500 hover:bg-indigo-400 w-full rounded text-white mt-5 py-3 px-8">
+                  Gabung Kelas
+                </button>
+                {/* <button className="bg-red-500 hover:bg-red-400 w-full rounded text-white mt-5 py-3">
+                  Tinggalkan Kelas
+                </button> */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-3 bg-white p-8 rounded shadow">
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            <b>INI TEXT BODY DARI DATABASE</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Debitis tempore deserunt nostrum
+            tenetur dolor. Possimus cupiditate, dolorem accusamus eius ullam
+            culpa fugiat ut sapiente recusandae fuga maiores quidem? Corporis,
+            maiores?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            tempore deserunt nostrum tenetur dolor. Possimus cupiditate, dolorem
+            accusamus eius ullam culpa fugiat ut sapiente recusandae fuga
+            maiores quidem? Corporis, maiores?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            tempore deserunt nostrum tenetur dolor. Possimus cupiditate, dolorem
+            accusamus eius ullam culpa fugiat ut sapiente recusandae fuga
+            maiores quidem? Corporis, maiores?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            tempore deserunt nostrum tenetur dolor. Possimus cupiditate, dolorem
+            accusamus eius ullam culpa fugiat ut sapiente recusandae fuga
+            maiores quidem? Corporis, maiores?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            tempore deserunt nostrum tenetur dolor. Possimus cupiditate, dolorem
+            accusamus eius ullam culpa fugiat ut sapiente recusandae fuga
+            maiores quidem? Corporis, maiores?
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
