a1fdcacecfafec12e2670aab7eb23705441aa881
ilhamhafidz404 <xxspanzie@gmail.com>
2024-12-09 00:14:41 +0700

    ICF-61: Fix active state and redirect NavLink on Sidebar Nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -82,28 +82,37 @@ export default function NavbarComponent() {
       >
         <ul className="text-center">
           <li className="mb-5">
-            <a
-              href=""
-              className="text-xl after:content-[''] after:bottom-0 after:h-[5px] after:bg-indigo-500 after:absolute after:rounded-full relative pb-2 hover:after:w-[5px] active:after:w-[15px] after:transition-all after:duration-500 after:left-1/2 after:-translate-x-1/2 after:w-full after:left-0 after:right-0 after:transition-all after:duration-500"
+            <Link
+              href="/"
+              className={`text-xl after:content-[''] after:bottom-0 after:h-[5px] after:bg-indigo-500 after:absolute after:rounded-full relative pb-2 hover:after:w-[5px] active:after:w-[15px] after:transition-all after:duration-500 after:left-1/2 after:-translate-x-1/2  ${
+                pathname == "/" &&
+                "after:w-full after:left-0 after:right-0 after:transition-all after:duration-500"
+              }`}
             >
               Home
-            </a>
+            </Link>
           </li>
           <li className="mb-5">
-            <a
-              href=""
-              className="text-xl after:content-[''] after:bottom-0 after:h-[5px] after:bg-indigo-500 after:absolute after:rounded-full relative pb-2 hover:after:w-[5px] active:after:w-[15px] after:transition-all after:duration-500 after:left-1/2 after:-translate-x-1/2"
+            <Link
+              href="/courses"
+              className={`text-xl after:content-[''] after:bottom-0 after:h-[5px] after:bg-indigo-500 after:absolute after:rounded-full relative pb-2 hover:after:w-[5px] active:after:w-[15px] after:transition-all after:duration-500 after:left-1/2 after:-translate-x-1/2  ${
+                pathname.startsWith("/courses") &&
+                "after:w-full after:left-0 after:right-0 after:transition-all after:duration-500"
+              }`}
             >
               Course
-            </a>
+            </Link>
           </li>
           <li className="mb-5">
-            <a
-              href=""
-              className="text-xl after:content-[''] after:bottom-0 after:h-[5px] after:bg-indigo-500 after:absolute after:rounded-full relative pb-2 hover:after:w-[5px] active:after:w-[15px] after:transition-all after:duration-500 after:left-1/2 after:-translate-x-1/2"
+            <Link
+              href="/teachers"
+              className={`text-xl after:content-[''] after:bottom-0 after:h-[5px] after:bg-indigo-500 after:absolute after:rounded-full relative pb-2 hover:after:w-[5px] active:after:w-[15px] after:transition-all after:duration-500 after:left-1/2 after:-translate-x-1/2  ${
+                pathname.startsWith("/teacher") &&
+                "after:w-full after:left-0 after:right-0 after:transition-all after:duration-500"
+              }`}
             >
               Teacher
-            </a>
+            </Link>
           </li>
           <li className="mt-10">
             <button
