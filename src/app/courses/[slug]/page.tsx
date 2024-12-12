4eb4bd0c4b8252e91cb782c58eadb12691c3ef45
ilhamhafidz404 <xxspanzie@gmail.com>
2024-12-13 00:20:33 +0700

    ICF-80: Show Submission Course on Detail Course

--- src/app/courses/[slug]/page.tsx
+++ src/app/courses/[slug]/page.tsx
@@ -5,10 +5,12 @@ import Modal from "@/components/Modal";
 import { useJoinCourse, useLeaveCourse } from "@/hooks/userCourse";
 import MasterLayout from "@/layouts/master";
 import { Course } from "@/models/Course";
+import { Submission } from "@/models/Submission";
 import formatDate from "@/tools/dateFormatter";
 import { getUserId } from "@/utils/getUserId";
 import { useQuery } from "@tanstack/react-query";
 import {
+  IconBookOpenFill,
   IconCalendar,
   IconCalendarFill,
   IconPeopleFill,
@@ -29,7 +31,7 @@ export default function DetailCoursePage({
   const { slug } = params;
 
   const [userId, setUserId] = useState<number | null>(0);
-  const [openModal, setOpenModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
 
   const { data, isLoading } = useQuery({
     queryKey: ["courses", slug, userId],
@@ -41,7 +43,7 @@ export default function DetailCoursePage({
   const mutationLeave = useLeaveCourse();
 
   const handleSubmitJoinCourse = async () => {
-    setOpenModal(!openModal)
+    setOpenModal(!openModal);
 
     mutationJoin.mutate({
       course_id: data?.data?.id || 0,
@@ -50,7 +52,7 @@ export default function DetailCoursePage({
   };
 
   const handleSubmitLeaveCourse = async () => {
-    setOpenModal(!openModal)
+    setOpenModal(!openModal);
 
     mutationLeave.mutate({
       course_id: data?.data?.id || 0,
@@ -81,78 +83,133 @@ export default function DetailCoursePage({
           </section>
           <section className="grid xl:grid-cols-4 mt-10 gap-5">
             <div className="relative">
-              <div className="border p-4 rounded bg-white sticky top-5 w-full">
-                <div className="flex gap-2 items-center mb-5">
-                  <span className="w-[55px] h-[55px] rounded-full bg-indigo-500"></span>
-                  <div>
-                    <p className="text-xl font-medium">
-                      {data?.data?.user.name}
-                    </p>
-                    <p className="text-sm text-gray-800 italic">
-                      {data?.data?.user.nuptk}
-                    </p>
+              <div className="sticky top-10">
+                <div className="border p-4 rounded bg-white w-full">
+                  <div className="flex gap-2 items-center mb-5">
+                    <span className="w-[55px] h-[55px] rounded-full bg-indigo-500"></span>
+                    <div>
+                      <p className="text-xl font-medium">
+                        {data?.data?.user.name}
+                      </p>
+                      <p className="text-sm text-gray-800 italic">
+                        {data?.data?.user.nuptk}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex xl:flex-col md:flex-row flex-col gap-5 justify-between xl:items-start items-center">
+                    <div className="w-full">
+                      <p className="text-sm italic text-gray-800 mb-5">
+                        <span className="flex gap-1 items-center">
+                          <IconCalendarFill className="size-5" />
+                          Dibuat Pada :
+                        </span>
+                        <span className="text-base text-gray-900 not-italic">
+                          {formatDate(
+                            data?.data?.created_at ||
+                              "2024-12-05T06:48:34.000000Z"
+                          )}
+                        </span>
+                      </p>
+                      <p className="text-sm italic text-gray-800">
+                        <span className="flex gap-1 items-center">
+                          <IconPeopleFill className="size-5" />
+                          Diikuti Oleh :
+                        </span>
+                        <span className="text-base text-gray-900 not-italic">
+                          200+ Siswa
+                        </span>
+                      </p>
+                    </div>
+                    <div className="w-full">
+                      {data.haveJoined ? (
+                        <button
+                          onClick={() => setOpenModal(!openModal)}
+                          className="bg-red-500 hover:bg-red-400 w-full rounded text-white mt-5 py-3"
+                        >
+                          Tinggalkan Kelas
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => setOpenModal(!openModal)}
+                          className="bg-indigo-500 hover:bg-indigo-400 w-full rounded text-white mt-5 py-3 px-8"
+                        >
+                          Gabung Kelas
+                        </button>
+                      )}
+                    </div>
                   </div>
                 </div>
-                <div className="flex xl:flex-col md:flex-row flex-col gap-5 justify-between xl:items-start items-center">
-                  <div className="w-full">
-                    <p className="text-sm italic text-gray-800 mb-5">
-                      <span className="flex gap-1 items-center">
-                        <IconCalendarFill className="size-5" />
-                        Dibuat Pada :
-                      </span>
-                      <span className="text-base text-gray-900 not-italic">
-                        {formatDate(
-                          data?.data?.created_at ||
-                            "2024-12-05T06:48:34.000000Z"
-                        )}
-                      </span>
-                    </p>
-                    <p className="text-sm italic text-gray-800">
-                      <span className="flex gap-1 items-center">
-                        <IconPeopleFill className="size-5" />
-                        Diikuti Oleh :
-                      </span>
-                      <span className="text-base text-gray-900 not-italic">
-                        200+ Siswa
-                      </span>
-                    </p>
-                  </div>
-                  <div className="w-full">
-                    {data.haveJoined ? (
-                      <button
-                        onClick={() => setOpenModal(!openModal)}
-                        className="bg-red-500 hover:bg-red-400 w-full rounded text-white mt-5 py-3"
-                      >
-                        Tinggalkan Kelas
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => setOpenModal(!openModal)}
-                        className="bg-indigo-500 hover:bg-indigo-400 w-full rounded text-white mt-5 py-3 px-8"
-                      >
-                        Gabung Kelas
-                      </button>
-                    )}
-                  </div>
+                <div className="bg-white mt-5 p-5 rounded border">
+                  <h4 className="font-medium mb-4 flex items-center gap-3">
+                    <IconBookOpenFill />
+                    Submissions
+                  </h4>
+                  <ul className="list-decimal pl-5">
+                    {data.data.submission.map((row: Submission) => (
+                      <li key={row.id} className="text-sm mb-2">
+                        <a href={`#ID${row.id}`}>{row.title}</a>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               </div>
             </div>
-            <div
-              className="xl:col-span-3 bg-white p-8 rounded shadow"
-              dangerouslySetInnerHTML={{ __html: data?.data?.body || "" }}
-            ></div>
+            <div className="xl:col-span-3">
+              <div className="bg-white p-5 rounded shadow w-full">
+                <div
+                  dangerouslySetInnerHTML={{ __html: data?.data?.body || "" }}
+                ></div>
+                <hr className="my-10" />
+                <div>
+                  {data.data.submission.map((row: Submission) => (
+                    <div className="mb-10 scroll-mt-20" id={`ID${row.id}`}>
+                      <h5 className="font-medium text-xl mb-2 flex items-center gap-3">
+                        <IconBookOpenFill />
+                        {row.title}
+                      </h5>
+                      <p className="mb-5 text-sm">
+                        Batas Akhir: {row.deadline}
+                      </p>
+                      <p className="mb-4">{row.description}</p>
+                      <h5 className="font-semibold">Jawab: </h5>
+                      <form action="">
+                        <textarea
+                          name=""
+                          id=""
+                          className="border rounded w-full h-[150px] p-2"
+                        ></textarea>
+                        <input type="file" />
+                        <button className="block bg-indigo-500 hover:bg-indigo-400 py-2 rounded w-full text-white mt-5">
+                          Submit
+                        </button>
+                      </form>
+                      <hr className="my-10" />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       )}
 
       <Modal
         isOpen={openModal}
-        message={data?.haveJoined ? 'Apakah anda yakin ingin keluar kelas?' : 'Apakah anda yakin ingin gabung kelas ini?'}
-        onConfirm={data?.haveJoined ? handleSubmitLeaveCourse : handleSubmitJoinCourse}
+        message={
+          data?.haveJoined
+            ? "Apakah anda yakin ingin keluar kelas?"
+            : "Apakah anda yakin ingin gabung kelas ini?"
+        }
+        onConfirm={
+          data?.haveJoined ? handleSubmitLeaveCourse : handleSubmitJoinCourse
+        }
         onClose={() => setOpenModal(!openModal)}
-        confirmBtnClassname={data?.haveJoined ? 'hover:bg-red-800 bg-red-500 text-white px-4 py-2 rounded' : 'hover:bg-indigo-800 bg-indigo-500 text-white px-4 py-2 rounded'}
+        confirmBtnClassname={
+          data?.haveJoined
+            ? "hover:bg-red-800 bg-red-500 text-white px-4 py-2 rounded"
+            : "hover:bg-indigo-800 bg-indigo-500 text-white px-4 py-2 rounded"
+        }
       />
-
     </MasterLayout>
   );
 }
